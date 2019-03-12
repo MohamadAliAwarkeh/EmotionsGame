03dae45a5f75d433d6bc45ef3a218f707e48b56d
MohamadAliAwarkeh <mohamad.awarkeh@gmail.com>
2019-03-12 09:04:24 +0000

    56456465

---
+++ NarrativePuzzleUnityProject/NarrativePuzzleUnityProject/Assets/RespawnZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnZone : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnZone;
+
+    private void OnTriggerEnter(Collider theCol)
+    {
+        if (theCol.gameObject.CompareTag("Player"))
+        {
+            theCol.gameObject.transform.position = respawnZone.position;
+        }
+    }
+}
